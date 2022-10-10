d2b2444a6fd98555d3f72c7a99e5ead1ad27b534
Yauheni.Hladki <yauheni_hladki@epam.com>
2022-10-10 07:21:34 +0200

    task-5: add import file

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://gnayp2mxbb.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://gnayp2mxbb.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://gnayp2mxbb.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://peeh55a333.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://gnayp2mxbb.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://gnayp2mxbb.execute-api.eu-west-1.amazonaws.com/dev",
 };
