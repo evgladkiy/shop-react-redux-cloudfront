e03fe8f610e6db7452e387577122d92f425ebb95
Yauheni.Hladki <yauheni_hladki@epam.com>
2022-12-09 03:09:27 +0100

    task-8: adjust to carts

--- src/components/MainLayout/components/Cart.tsx
+++ src/components/MainLayout/components/Cart.tsx
@@ -5,9 +5,10 @@ import { Link } from "react-router-dom";
 import { useCart } from "~/queries/cart";
 
 export default function Cart() {
-  const { data = [] } = useCart();
-  const badgeContent = data.length || undefined;
+  const { data = {} } = useCart();
+  const items = data.data || 0; 
 
+  const badgeContent = items.length || undefined;
   return (
     <IconButton color="inherit" component={Link} to="/cart" size="large">
       <Badge badgeContent={badgeContent} color="secondary">
